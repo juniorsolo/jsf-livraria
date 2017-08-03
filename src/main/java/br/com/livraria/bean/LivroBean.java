26c0053ae09ea0f39a0609f9b89bdd8fdd55d132
juniorsolo <junior.solo@hotmail.com>
2017-08-03 20:27:18 -0300

    Criando bean.

---
+++ src/main/java/br/com/livraria/bean/LivroBean.java
@@ -0,0 +1,11 @@
+package br.com.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	public void gravar() {
+		System.out.println("Gravando Livro");
+	}
+}
